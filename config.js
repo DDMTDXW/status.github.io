0d3bd65e618d551b5627c29224027bcf0c9cace6
DDMTDXWW <t15913896969@163.com>
2022-07-20 19:42:36 +0800

    first commit

---
+++ config.js
@@ -0,0 +1,45 @@
+﻿// 配置
+window.Config = {
+
+  // 站点名
+  SiteName: 'Uptime Status',
+
+  // 站点链接
+  SiteUrl: '/',
+
+  // UptimeRobot Api 域名
+  // 只需填写域名部分，默认为官网域名
+  // 因官网 API 时不时的会 CROS 报错，可填自定义反代域名
+  // 详见 https://github.com/yb/uptime-status/ 说明
+  ApiDomain: 'api.uptimerobot.com',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only 两只 Api Key
+  ApiKeys: [
+    'm791826177-cc3346fed5e703681f3136d8',
+    'm791826270-98d633a8d59d95c2bb92e312',
+    'm791826282-65a4f5f305e99b0a2254d9f8',
+    'm791826278-66bfdc0240d690ee813b4630',
+    'm791826274-dcdf0b9ae879abaf614df1ea',
+  ],
+
+  // 是否显示监测站点的链接
+  ShowLink: true,
+
+  // 日志天数
+  // 虽然免费版说仅保存60天日志，但测试好像API可以获取90天的
+  // 不过时间不要设置太长，容易卡，接口请求也容易失败
+  CountDays: 60,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Homepage',
+      url: 'https://status.org.cn/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/yb/uptime-status'
+    }
+  ]
+};
